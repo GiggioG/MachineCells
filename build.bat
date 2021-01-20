2a9439e3813c63825c1955a79079c29b30bce6c4
GiggioG <dimitrstanimirov@gmail.com>
2021-01-20 15:02:42 +0200

    fix and improve build script

--- build.bat
+++ build.bat
@@ -1,2 +1,12 @@
+@echo off
+echo VVVVVVVVVV-BUILD-LOG-VVVVVVVVVV
 g++ main.cpp -o main.exe -std=c++11
+echo AAAAAAAAAA-BUILD-LOG-AAAAAAAAAA
+echo COMPILATION ERRORLEVEL: %errorlevel%
+if %errorlevel% EQU 0 (
+echo COMPILATION COMPLETE: RUNNING main.exe
 start main.exe
+)
+if %errorlevel% NEQ 0 (
+echo ERROR DURING COMPILATION
+)

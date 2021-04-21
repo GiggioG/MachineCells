a5a728d6a326bd1078ab690d68eb69fc4c4107bb
GiggioG <dimitrstanimirov@gmail.com>
2021-04-21 12:25:24 +0300

    add 3x3 charmaps for cells

--- main.cpp
+++ main.cpp
@@ -2,20 +2,41 @@
 #include "colors.cpp"
 using namespace std;
 //config consts
-const unsigned char MOVER_CH[4] = {'^','>','v','<'};
+
+const unsigned char MOVER_CH_UP = '^';
+const unsigned char MOVER_CH_RIGHT = '>';
+const unsigned char MOVER_CH_DOWN = 'v';
+const unsigned char MOVER_CH_LEFT = '<';
+const unsigned char MOVER_CH[4] = {MOVER_CH_UP,MOVER_CH_RIGHT,MOVER_CH_DOWN,MOVER_CH_LEFT};
+
 const COLORS MOVER_COL = BLUE;
 const unsigned char STATIC_CH = 178;
 const COLORS STATIC_COL = LIGHT_GREY;
 const unsigned char ENEMY_CH = 234;
 const COLORS ENEMY_COL = RED;
-const unsigned char ROTATOR_CH[2] = {149,162};
+
+const unsigned char ROTATOR_CH_CLCW = 142;
+const unsigned char ROTATOR_CH_CTCLCW = 162;
+const unsigned char ROTATOR_CH[2] = {ROTATOR_CH_CLCW, ROTATOR_CH_CTCLCW};
+
 const COLORS ROTATOR_COL = BROWN;
-const unsigned char CLONER_CH[4] = {30,16,31,17};
+
+const unsigned char CLONER_CH_UP = 30;
+const unsigned char CLONER_CH_RIGHT = 16;
+const unsigned char CLONER_CH_DOWN = 31;
+const unsigned char CLONER_CH_LEFT = 17;
+const unsigned char CLONER_CH[4] = {CLONER_CH_UP, CLONER_CH_RIGHT, CLONER_CH_DOWN, CLONER_CH_LEFT};
+
 const COLORS CLONER_COL = GREEN;
 const unsigned char PUSHABLE_CH = '#';
-const unsigned char UNIDIRECTIONAL_CH[2] = {205,186};
+
+const unsigned char UNIDIRECTIONAL_CH_HORZ = 205;
+const unsigned char UNIDIRECTIONAL_CH_VERT = 186;
+const unsigned char UNIDIRECTIONAL_CH[2] = {UNIDIRECTIONAL_CH_HORZ, UNIDIRECTIONAL_CH_VERT};
+
 const COLORS PUSHABLE_COL = YELLOW;
 const unsigned char EMPTY_CH = '.';
+
 const COLORS CURSOR_FG_COL = PINK;
 const COLORS CURSOR_BG_COL = WHITE;
 const COLORS BLINK_FG_1 = RED;
@@ -97,7 +118,7 @@ struct GameLevel {
     int br_enemies_copy = 0;
     int rOff, cOff;
 
-    void drawChar(uchar ch, int y, int x, COLORS foreground_color, COLORS background_color);
+    void drawCell(uchar ch, int y, int x, COLORS foreground_color, COLORS background_color);
     bool push(int r, int c, int d);
     bool readFromFile(string path);
     void initLevelFromData();
@@ -111,9 +132,136 @@ struct GameLevel {
     bool blinkMessage(string msg);
 };
 
+typedef const char CharMap[3][4];
+CharMap MOVER_UP_CMP = {
+    " ^ ",
+    "/_\\",
+    "^^^"
+};
+CharMap MOVER_RIGHT_CMP = {
+    ">\\ ",
+    ">|>",
+    ">/ "
+};
+CharMap MOVER_DOWN_CMP = {
+    "vvv",
+    "\\-/",
+    " v "
+};
+CharMap MOVER_LEFT_CMP = {
+    " /<",
+    "<|<",
+    " \\<"
+};
+CharMap PUSHABLE_CMP = {
+    "+-+",
+    "| |",
+    "+-+"
+};
+CharMap ENEMY_CMP = {
+    ". .",
+    " _ ",
+    "/ \\"
+};
+CharMap ROTATOR_CLCW_CMP = {
+    "/-\\",
+    "| v",
+    "\\-/"
+};
+CharMap ROTATOR_CTCLCW_CMP = {
+    "/-\\",
+    "v |",
+    "\\-/"
+};
+CharMap CLONER_UP_CMP = {
+    "/^\\",
+    " | ",
+    " \xc1 "
+};
+CharMap CLONER_RIGHT_CMP = {
+    "  \\",
+    "\xc3->",
+    "  /"
+};
+CharMap CLONER_DOWN_CMP = {
+    " \xc2 ",
+    " | ",
+    "\\v/"
+};
+CharMap CLONER_LEFT_CMP = {
+    "/  ",
+    "<-\xb4",
+    "\\  "
+};
+CharMap UNIDIRECTIONAL_HORZ_CMP = {
+    "---",
+    "\xcd\xcd\xcd",
+    "---"
+};
+CharMap UNIDIRECTIONAL_VERT_CMP = {
+    "|\xba|",
+    "|\xba|",
+    "|\xba|"
+};
+CharMap STATIC_CMP = {
+    "\xda\xc4\xbf",
+    "\xb3 \xb3",
+    "\xc0\xc4\xd9"
+};
 
-void GameLevel::drawChar(uchar ch, int y, int x, COLORS foreground_color, COLORS background_color) {
-    draw_char(ch, y + rOff, x + cOff, foreground_color, background_color);
+const int CELL_H_SIZE = 4;
+const int CELL_V_SIZE = 4;
+
+void GameLevel::drawCell(uchar ch, int row, int col, COLORS foreground_color, COLORS background_color) {   
+    CharMap* charMap;
+
+    switch (ch) {
+        case MOVER_CH_UP: charMap = &MOVER_UP_CMP; break;
+        case MOVER_CH_RIGHT: charMap = &MOVER_RIGHT_CMP; break;
+        case MOVER_CH_DOWN: charMap = &MOVER_DOWN_CMP; break;
+        case MOVER_CH_LEFT: charMap = &MOVER_LEFT_CMP; break;
+        case PUSHABLE_CH: charMap = &PUSHABLE_CMP; break;
+        case ENEMY_CH: charMap = &ENEMY_CMP; break;
+        case ROTATOR_CH_CLCW: charMap = &ROTATOR_CLCW_CMP; break;
+        case ROTATOR_CH_CTCLCW: charMap = &ROTATOR_CTCLCW_CMP; break;
+        case CLONER_CH_UP: charMap = &CLONER_UP_CMP; break;
+        case CLONER_CH_RIGHT: charMap = &CLONER_RIGHT_CMP; break;
+        case CLONER_CH_DOWN: charMap = &CLONER_DOWN_CMP; break;
+        case CLONER_CH_LEFT: charMap = &CLONER_LEFT_CMP; break;
+        case UNIDIRECTIONAL_CH_HORZ: charMap = &UNIDIRECTIONAL_HORZ_CMP; break;
+        case UNIDIRECTIONAL_CH_VERT: charMap = &UNIDIRECTIONAL_VERT_CMP; break;
+        case STATIC_CH: charMap = &STATIC_CMP; break;
+        default: charMap = nullptr;
+    }
+
+    const char H_LINE[] = { '\xda', '\xc4', '\xc4', '\xc4' };
+    const char V = '\xb3';
+
+    row = CELL_H_SIZE * row + rOff;
+    col = CELL_V_SIZE * col + cOff;
+
+    for(int i = 0; i < 4; i++){
+        draw_char(H_LINE[i], row, col+i, WHITE, BLACK);
+    }
+    for(int i = 0; i < 3; i++){
+        draw_char(V, row + 1 + i, col, WHITE, BLACK);
+    }
+    row++;
+    col++;
+
+    if(charMap == nullptr){
+        for(int r = 0; r < CELL_V_SIZE - 1; r++){
+            for(int c = 0; c < CELL_H_SIZE - 1; c++){
+                draw_char(ch, row + r, col + c, foreground_color, background_color);
+            }
+        }
+    } else {
+        for(int r = 0; r < CELL_V_SIZE - 1; r++){
+            for(int c = 0; c < CELL_H_SIZE - 1; c++){
+                draw_char((*charMap)[r][c], row + r, col + c, foreground_color, background_color);
+            }
+        }
+    }
 }
 
 bool GameLevel::push(int r, int c, int d){
@@ -236,7 +384,7 @@ void GameLevel::printField(){
                 fg = BKG_COL;
                 bg = field[r][c].col;
             }
-            drawChar(field[r][c].ch, r, c, fg, bg);
+            drawCell(field[r][c].ch, r, c, fg, bg);
         }
     }
 }
@@ -248,9 +396,9 @@ void GameLevel::edit(){
     while (true) {
         printField();
         if (handFull) {
-            drawChar(hand.ch, r_cur, c_cur, CURSOR_FG_COL, CURSOR_BG_COL);
+            drawCell(hand.ch, r_cur, c_cur, CURSOR_FG_COL, CURSOR_BG_COL);
         } else {
-            drawChar(field[r_cur][c_cur].ch, r_cur, c_cur, field[r_cur][c_cur].col, CURSOR_BG_COL);
+            drawCell(field[r_cur][c_cur].ch, r_cur, c_cur, field[r_cur][c_cur].col, CURSOR_BG_COL);
         }
         if (GetAsyncKeyState('W') && (r_cur > min_r)) {
             r_cur--;
@@ -302,16 +450,22 @@ bool GameLevel::blinkMessage(string msg) {
        |___________________ 
     */
 
-    int row = rOff + rows/2;
-    int col = cOff + (cols - msg.length())/2;
+    msg = ' ' + msg + ' ';
+
+    int row = rOff + (CELL_H_SIZE * rows)/2;
+    int col = cOff + (CELL_V_SIZE * cols)/2 - msg.length()/2;
 
     bool variant = false;
     while (true) {
         for (int i = 0; i < msg.length(); i++) {
             if (variant) {
+                draw_char(' ', row - 1, col + i, BLINK_FG_1, BLINK_BG_1);
                 draw_char(msg[i], row, col + i, BLINK_FG_1, BLINK_BG_1);
+                draw_char(' ', row + 1, col + i, BLINK_FG_1, BLINK_BG_1);
             } else {
+                draw_char(' ', row - 1, col + i, BLINK_FG_2, BLINK_BG_2);
                 draw_char(msg[i], row, col + i, BLINK_FG_2, BLINK_BG_2);
+                draw_char(' ', row + 1, col + i, BLINK_FG_2, BLINK_BG_2);
             }
         }
         variant = !variant;
